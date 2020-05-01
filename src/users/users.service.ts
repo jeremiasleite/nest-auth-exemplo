06da832368b0965741486655df246c435959ea28
jeremiasleite <jeremias23456@gmail.com>
2020-05-01 17:31:56 -0300

    add exception not found to find user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,44 +4,54 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { CreateUserDto } from './dto/CreateUserDto';
 import { UpdateUserDto } from './dto/update-userDto';
+import { CostumeNotFoundException } from 'src/exceptions/notfound.exception';
 
 @Injectable()
 export class UsersService {
-  
+
   constructor(
     @InjectRepository(User)
     private usersRepository: Repository<User>,
   ) { }
 
-  create(createUserDto: CreateUserDto): Promise<User>{
+  create(createUserDto: CreateUserDto): Promise<User> {
     return this.usersRepository.save(createUserDto);
   }
 
   findAll(): Promise<User[]> {
     return this.usersRepository.find();
   }
-  findOne(id: string): Promise<User> {
-    return this.usersRepository.findOne(id);
+  
+  async findOne(id: string): Promise<User> {
+    const usuario = this.usersRepository.findOne(id).then(e => new User(e));
+    if (!(await usuario).id) {
+      throw new CostumeNotFoundException("Usuário não encontrado.")
+    }
+    return usuario
   }
 
   async remove(id: string): Promise<void> {
     await this.usersRepository.delete(id);
   }
 
-  async findOneUserName(username: string): Promise<User | undefined> {
-    return this.usersRepository.findOne({username: username});
+  async findOneUserName(username: string): Promise<User> {
+    const usuario = this.usersRepository.findOne({ username: username }).then(e => new User(e));
+    if (!(await usuario).id) {
+      throw new CostumeNotFoundException("Usuário não encontrado.")
+    }
+    return usuario
   }
 
   async update(usuario: UpdateUserDto): Promise<User> {
     const usuarioToUpdate = await this.usersRepository.findOne(usuario.id).then(e => new User(e));
-    if( !(await usuarioToUpdate).id){
-        //throw new CostumeNotFoundException("Usuário não encontrado.")
+    if (!(await usuarioToUpdate).id) {
+      throw new CostumeNotFoundException("Usuário não encontrado.")
     }
     usuarioToUpdate.username = usuario.username
     usuarioToUpdate.email = usuario.email
     usuarioToUpdate.isActive = usuario.isActive
-     
+
     return this.usersRepository.save(usuarioToUpdate)
-}
+  }
 
 }
